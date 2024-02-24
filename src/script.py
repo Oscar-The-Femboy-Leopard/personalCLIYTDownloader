a0d08ab8f0bf165ea6f0aa843138235c82ea95f4
SpiritingAuto04 <jeffmann013@gmail.com>
2024-02-24 00:51:56 +0000

    Updated script, added licence and reworked directory

--- src/script.py
+++ src/script.py
@@ -4,6 +4,32 @@ from pytube import YouTube
 from pytube.cli import on_progress
 from pytube.helpers import safe_filename
 from datetime import timedelta
+from enum import Enum
+
+if not os.path.exists("YT Downloads"):
+    os.mkdir("YT Downloads")
+
+OP: str = "YT Downloads"
+
+
+white = ""
+red = "\033[31m"
+green = "\n\033[32m"
+
+
+class ReturnCodes(Enum):
+    OK = 0
+    Back = 1
+    UserQuit = 2
+
+
+def codes(value: str):
+    if value == "quit":
+        return ReturnCodes.UserQuit
+    if value == "back":
+        return ReturnCodes.Back
+
+    return ReturnCodes.OK
 
 
 def mainRun():
@@ -11,10 +37,14 @@ def mainRun():
         option = input(str("Video or Audio?\n>> ")).lower()
 
         if option == 'video':
-            VidDownload()
+            ret = VidDownload()
 
         if option == 'audio':
-            AudDownload()
+            ret = AudDownload()
+
+        if ret is ReturnCodes.UserQuit:
+            print(f"{red}You have exited the program. Have a great day!")
+            break
 
 
 def prettify_numb(number: str) -> str:
@@ -23,7 +53,7 @@ def prettify_numb(number: str) -> str:
 
     count = 0
 
-    for index in range(len(token_list)-1, -1, -1):
+    for index in range(len(token_list) - 1, -1, -1):
         token = token_list[index]
 
         formatted.append(token)
@@ -40,93 +70,75 @@ def prettify_numb(number: str) -> str:
 
 
 def VidDownload():
-        link = input(str("Enter the link for the youtube video you want to download:\n>> "))
+    link = input(str("Enter the link for the youtube video you want to download:\n>> "))
 
-        if link == "exit":
-            return print("\033[31mYou have exited the program. Have a great day!")
+    if link == "exit":
+        return ReturnCodes.UserQuit
 
-        yt = YouTube(link, use_oauth=True, on_progress_callback=on_progress)
-        ys = yt.streams
+    if link == "back":
+        return ReturnCodes.Back
 
-        os.chdir(os.path.join(os.environ['USERPROFILE'], 'Downloads'))
-        os.listdir()
+    yt = YouTube(link, use_oauth=True, on_progress_callback=on_progress)
+    ys = yt.streams
 
-        if 'YT Downloads' in os.listdir():
-            op = './YT Downloads/'
+    os.chdir(os.path.join(os.environ['USERPROFILE'], 'Downloads'))
+    os.listdir()
 
-        else:
-            os.mkdir('YT Downloads')
-            op = './YT Downloads/'
+    info = f'{safe_filename(yt.author + " - " + yt.title)}.mp4'
 
-        if yt.length > 360:
-            return print("\033[31mThis file is over the limit of 6 minutes.\033[37m")
+    if info in OP:
+        return print("This video is already in the folder. Please choose another.")
 
-        info = f'{safe_filename(yt.author + " - " + yt.title)}.mp4'
+    views = prettify_numb(f"{yt.views}")
+    dur = timedelta(seconds=yt.length)
 
-        if info in op:
-            return print("This video is already in the folder. Please choose another.")
+    print(f"{green}Successful! Found the video. Commencing download.")
+    print("\033[37mTitle: ", yt.title)
+    print("Uploader: ", yt.author)
+    print("Number of views: ", views)
+    print("Length of video: ", dur)
 
-        views = prettify_numb(f"{yt.views}")
-        dur = timedelta(seconds=yt.length)
+    download = ys.get_highest_resolution()
+    download.download(output_path=OP, filename=f"{info}", max_retries=3)
 
-        print("\n\033[32mSuccessful! Found the video. Commencing download.")
-        print("\033[37mTitle: ", yt.title)
-        print("Uploader: ", yt.author)
-        print("Number of views: ", views)
-        print("Length of video: ", dur)
-
-        download = ys.get_highest_resolution()
-        download.download(output_path=op, filename=f"{info}", max_retries=3)
-
-        print(f"\033[32m→Successful!")
-        print(f"\033[37m     ↪Saved in Downloads: {op}{info}")
-        print(f"     File Size: {download.filesize_kb}kb || {download.filesize_mb}mb")
-        print("-"*40 + "\n")
+    print(f"\033[32m→Successful!")
+    print(f"\033[37m     ↪Saved in Downloads: {OP}{info}")
+    print(f"     File Size: {download.filesize_kb}kb || {download.filesize_mb}mb")
+    print("-" * 40 + "\n")
 
 
 def AudDownload():
-        link = input(str("Enter the link for the youtube video audio you want to download:\n>> "))
+    link = input(str("Enter the link for the youtube video audio you want to download:\n>> "))
 
-        if link == "exit":
-            return print("\033[31mYou have exited the program. Have a great day!")
+    if link == "exit":
+        return print("\033[31mYou have exited the program. Have a great day!")
 
-        yt = YouTube(link, use_oauth=True, on_progress_callback=on_progress)
-        ys = yt.streams.filter(only_audio=True).first()
+    yt = YouTube(link, use_oauth=True, on_progress_callback=on_progress)
+    ys = yt.streams.filter(only_audio=True).first()
 
-        os.chdir(os.path.join(os.environ['USERPROFILE'], 'Downloads'))
-        os.listdir()
+    os.chdir(os.path.join(os.environ['USERPROFILE'], 'Downloads'))
 
-        if 'YT Downloads' in os.listdir():
-            op = './YT Downloads/'
+    info = f'{safe_filename(yt.author + " - " + yt.title)}.mp3'
 
-        else:
-            os.mkdir('YT Downloads')
-            op = './YT Downloads/'
+    if info in OP:
+        return print("This video is already in the folder. Please choose another.")
 
-        if yt.length > 360:
-            return print("\033[31mThis file is over the limit of 6 minutes.")
+    views = prettify_numb(f"{yt.views}")
+    dur = timedelta(seconds=yt.length)
 
-        info = f'{safe_filename(yt.author + " - " + yt.title)}.mp3'
+    print("\n\033[32mSuccessful! Found the video. Commencing download.")
+    print("\033[37mTitle: ", yt.title)
+    print("Uploader: ", yt.author)
+    print("Number of views: ", views)
+    print("Length of video: ", dur)
 
-        if info in op:
-            return print("This video is already in the folder. Please choose another.")
+    # download = ys.get_highest_resolution()
+    ys.download(output_path=OP, filename=f"{info}", max_retries=3)
 
-        views = prettify_numb(f"{yt.views}")
-        dur = timedelta(seconds=yt.length)
-
-        print("\n\033[32mSuccessful! Found the video. Commencing download.")
-        print("\033[37mTitle: ", yt.title)
-        print("Uploader: ", yt.author)
-        print("Number of views: ", views)
-        print("Length of video: ", dur)
-
-        # download = ys.get_highest_resolution()
-        ys.download(output_path=op, filename=f"{info}", max_retries=3)
-
-        print(f"\033[32m→Successful!")
-        print(f"\033[37m     ↪Saved in Downloads: {op}{info}")
-        print(f"     File Size: {ys.filesize_kb}kb || {ys.filesize_mb}mb")
-        print("-"*40 + "\n")
+    print(f"\033[32m→Successful!")
+    print(f"\033[37m     ↪Saved in Downloads: {OP}{info}")
+    print(f"     File Size: {ys.filesize_kb}kb || {ys.filesize_mb}mb")
+    print("-" * 40 + "\n")
 
 
 if __name__ in '__main__':
